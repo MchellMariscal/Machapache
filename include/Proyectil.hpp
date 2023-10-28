5c0a50b247e896a783c00e0229eb1dd322f89360
Valeria Michell Flores Mariscal <143460008+MchellMariscal@users.noreply.github.com>
2023-10-28 18:21:38 +0000

    Listo

--- include/Proyectil.hpp
+++ include/Proyectil.hpp
@@ -1,20 +1,24 @@
 #pragma once
-#include<Dibujo.hpp>
-#include<Actualizable.hpp>
+#include <Dibujo.hpp>
+#include <Actualizable.hpp>
 
 class Proyectil : public Dibujo, public Actualizable
 {
 private:
     int direccion;
+
 public:
-    Proyectil(int x, int y) : Dibujo(x, y, "bala") {
-        this->direccion =1;
-    } 
-    ~Proyectil() {} 
-    void CambiarDireccion(){
-        this->direccion *=-1;
+    Proyectil(int x, int y) : Dibujo(x, y, "bala")
+    {
+        this->direccion = 1;
     }
-    void Actualizar(){
-        this->x +=this->direccion;
+    ~Proyectil() {}
+    void CambiarDireccion()
+    {
+        this->direccion *= -1;
+    }
+    void Actualizar()
+    {
+        this->x += this->direccion;
     }
 };
