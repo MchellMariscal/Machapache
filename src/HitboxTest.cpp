7a2c36911d7c75c23ae83f97a6bcde56179c31fa
NestorMorales271 <a22310271@live.ceti.mx>
2023-12-03 22:08:48 -0600

    Pruba 2 de hitbox funciona (Posiciones)

--- src/HitboxTest.cpp
+++ src/HitboxTest.cpp
@@ -11,38 +11,38 @@ public:
 
         // Cargar la imagen desde un archivo
         
-        if (!texture.loadFromFile("assets/Images/TestSources/pikachu.png")){
+        // if (!texture.loadFromFile("assets/Images/TestSources/pikachu.png")){
         
-        }
-        this->sprite = sf::Sprite(texture);
-        this->sprite.setPosition(position); // Posición inicial sprite
+        // }
+        // this->sprite = sf::Sprite(texture);
+        // this->sprite.setPosition(position); // Posición inicial sprite
     }
 
     void move(float offsetX, float offsetY)
     {
-        sprite.move(offsetX, offsetY);
+        // sprite.move(offsetX, offsetY);
         shape.move(offsetX, offsetY);
     }
 
     void draw(sf::RenderWindow &window)
     {
         window.draw(this->shape);
-        window.draw(this->sprite);
+        // window.draw(this->sprite);
     }
 
-    void update(){
-        // Actualizar el frame de la animación
-        if (clock.getElapsedTime().asSeconds() >= frameTime)
-        {
-            currentFrame = (currentFrame + 1) % numFrames;
-            sprite.setTextureRect(sf::IntRect((currentFrame * 64)+17, 133, 64, 36));
-            clock.restart();
-        }
-    }
+    // void update(){
+    //     // Actualizar el frame de la animación
+    //     if (clock.getElapsedTime().asSeconds() >= frameTime)
+    //     {
+    //         currentFrame = (currentFrame + 1) % numFrames;
+    //         sprite.setTextureRect(sf::IntRect((currentFrame * 64)+17, 133, 64, 36));
+    //         clock.restart();
+    //     }
+    // }
 
 private:
     sf::RectangleShape shape;
-    sf::Sprite sprite;
+    // sf::Sprite sprite;
     sf::Texture texture;
     sf::Clock clock;
     float frameTime = 0.1f; // Tiempo entre cada frame en segundos
@@ -52,17 +52,24 @@ private:
     int frameHeight = 32;
 };
 
-double velocidad = 0.1;
+double velocidad = 0.2;
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(800, 600), "Hitbox Test");
 
-    Personaje pika(sf::Vector2f(400, 300), sf::Color::Red);
-    Personaje player(sf::Vector2f(450, 550), sf::Color::Green);
-    Personaje enemy1(sf::Vector2f(550, 50), sf::Color::White);
-    Personaje enemy2(sf::Vector2f(450, 50), sf::Color::White);
-    Personaje enemy3(sf::Vector2f(350, 50), sf::Color::White);
+    Personaje pika(sf::Vector2f(10, 10), sf::Color::Red);
+    Personaje player(sf::Vector2f(350, 525), sf::Color::Green);
+    Personaje enemy1(sf::Vector2f(150, 100), sf::Color::White);
+    Personaje enemy2(sf::Vector2f(350, 100), sf::Color::White);
+    Personaje enemy3(sf::Vector2f(550, 100), sf::Color::White);
+    Personaje enemy4(sf::Vector2f(75, 175), sf::Color::White);
+    Personaje enemy5(sf::Vector2f(275, 175), sf::Color::White);
+    Personaje enemy6(sf::Vector2f(475, 175), sf::Color::White); 
+    Personaje enemy7(sf::Vector2f(675, 175), sf::Color::White);    
+    Personaje enemy8(sf::Vector2f(150, 250), sf::Color::White);
+    Personaje enemy9(sf::Vector2f(350, 250), sf::Color::White);
+    Personaje enemy10(sf::Vector2f(550, 250), sf::Color::White);
 
     while (window.isOpen())
     {
@@ -74,7 +81,8 @@ int main()
                 window.close();
             }
         }
-
+        pika.move(velocidad, 0);
+        
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
         {
             player.move(velocidad * -1, 0);
@@ -83,21 +91,21 @@ int main()
         {
             player.move(velocidad, 0);
         }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-        {
-            pika.move(0, velocidad * -1);
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-        {
-            pika.move(0, velocidad);
-        }
+        // if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+        // {
+        //     pika.move(0, velocidad * -1);
+        // }
+        // if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+        // {
+        //     pika.move(0, velocidad);
+        // }
 
         // Actualizar animacion pikachu
-        pika.update();
-        player.update();
-        enemy1.update();
-        enemy2.update();
-        enemy3.update();
+        // pika.update();
+        // player.update();
+        // enemy1.update();
+        // enemy2.update();
+        // enemy3.update();
 
         window.clear();
         pika.draw(window);
@@ -105,6 +113,13 @@ int main()
         enemy1.draw(window);
         enemy2.draw(window);
         enemy3.draw(window);
+        enemy4.draw(window);
+        enemy5.draw(window);
+        enemy6.draw(window);
+        enemy7.draw(window);
+        enemy8.draw(window);
+        enemy9.draw(window);
+        enemy10.draw(window);
         window.display();
     }
 
