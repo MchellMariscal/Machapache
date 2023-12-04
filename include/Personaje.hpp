14788aaa082ce4310128ba0143b364907fcc20f6
NestorMorales271 <a22310271@live.ceti.mx>
2023-12-03 22:19:30 -0600

    Se exporta la clase personaje de hitbox

---
+++ include/Personaje.hpp
@@ -0,0 +1,52 @@
+#include <SFML/Graphics.hpp>
+class Personaje
+{
+public:
+    Personaje(sf::Vector2f position, sf::Color color)
+    {
+        shape.setSize(sf::Vector2f(50, 50));
+        shape.setPosition(position); // Posición inicial cuadro
+        shape.setFillColor(color);
+
+        // Cargar la imagen desde un archivo
+        
+        // if (!texture.loadFromFile("assets/Images/TestSources/pikachu.png")){
+        
+        // }
+        // this->sprite = sf::Sprite(texture);
+        // this->sprite.setPosition(position); // Posición inicial sprite
+    }
+
+    void move(float offsetX, float offsetY)
+    {
+        // sprite.move(offsetX, offsetY);
+        shape.move(offsetX, offsetY);
+    }
+
+    void draw(sf::RenderWindow &window)
+    {
+        window.draw(this->shape);
+        // window.draw(this->sprite);
+    }
+
+    // void update(){
+    //     // Actualizar el frame de la animación
+    //     if (clock.getElapsedTime().asSeconds() >= frameTime)
+    //     {
+    //         currentFrame = (currentFrame + 1) % numFrames;
+    //         sprite.setTextureRect(sf::IntRect((currentFrame * 64)+17, 133, 64, 36));
+    //         clock.restart();
+    //     }
+    // }
+
+private:
+    sf::RectangleShape shape;
+    // sf::Sprite sprite;
+    sf::Texture texture;
+    sf::Clock clock;
+    float frameTime = 0.1f; // Tiempo entre cada frame en segundos
+    int currentFrame = 0;
+    int numFrames = 4; // Número total de frames en la animación
+    int frameWidth = 32;
+    int frameHeight = 32;
+};
